410e697712805906f8b4d5bf52ba71ec963d225a
avong886 <alexvong8@gmail.com>
2016-02-23 13:58:38 -0800

    changed light

--- 01-rotating-mesh/js/main.js
+++ 01-rotating-mesh/js/main.js
@@ -29,8 +29,9 @@ function initRenderer() {
 }
 
 function initLights() {
-    var light = new THREE.AmbientLight(0x66ffcc);
-    scene.add(light);
+    var light = new THREE.PointLight( 0xff0000, 1, 100 ); 
+    light.position.set( 50, 50, 50 ); 
+    scene.add( light );
 }
 
 var mesh = null;
