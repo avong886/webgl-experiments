589d730ff4856d868caa568cf337ce3751d38f14
avong886 <alexvong8@gmail.com>
2016-02-24 14:25:59 -0800

    Update main.js

--- 01-rotating-mesh/js/main.js
+++ 01-rotating-mesh/js/main.js
@@ -66,7 +66,7 @@ plane.position.set(0,0,-2);
 function initCamera() {
     camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 100);
     camera.position.set(0, 3.5, 5);
-    camera.lookAt(mesh.position);
+    camera.lookAt(scene.position);
 }
 
 
