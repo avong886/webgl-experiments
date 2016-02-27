cabfa416329438cc4e8a70911946d7361ce1e6e2
avong886 <alexvong8@gmail.com>
2016-02-26 20:01:26 -0800

    Update main.js

--- 01-rotating-mesh/js/main.js
+++ 01-rotating-mesh/js/main.js
@@ -152,18 +152,18 @@ function initRenderer() {
 
 function initLights() {
     var light = new THREE.PointLight( 0xffffff, 5, 100 ); 
-    light.position.set( 50, 50, 50 ); 
+    light.position.set( 10, 10, 20 ); 
     scene.add( light );
 
 var light2 = new THREE.PointLight( 0xffff66, 1, 100 ); 
     light2.position.set( -5, 20, 10 ); 
     scene.add( light2 );
 
-spotLight = new THREE.SpotLight(0xffffff);
-spotLight.position.set(0, 0, 460);
-spotLight.intensity = 1;
-spotLight.castShadow = true;
-scene.add(spotLight);
+//spotLight = new THREE.SpotLight(0xffffff);
+//spotLight.position.set(0, 0, 460);
+//spotLight.intensity = 1;
+//spotLight.castShadow = true;
+//scene.add(spotLight);
 
 }
 
